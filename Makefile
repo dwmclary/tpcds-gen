6ebb48a3a83effa7093d4fbae67cb4a47812099e
Dan McClary <dan.mcclary@oracle.com>
2014-04-17 13:22:43 -0700

    modified for MR based parallel gen

--- Makefile
+++ Makefile
@@ -4,12 +4,12 @@ all: target/lib/dsdgen.jar target/tpcds-gen-1.0-SNAPSHOT.jar
 target/tpcds-gen-1.0-SNAPSHOT.jar: $(shell find -name *.java) 
 	mvn package
 
-target/tpcds_kit.zip: tpcds_kit.zip
-	mkdir -p target/
-	cp tpcds_kit.zip target/tpcds_kit.zip
+#target/tpcds_kit.zip: tpcds_kit.zip
+#	mkdir -p target/
+#	cp tpcds_kit.zip target/tpcds_kit.zip
 
-tpcds_kit.zip:
-	curl --output tpcds_kit.zip http://www.tpc.org/tpcds/dsgen/dsgen-download-files.asp?download_key=NaN
+#tpcds_kit.zip:
+#	curl --output tpcds_kit.zip http://www.tpc.org/tpcds/dsgen/dsgen-download-files.asp?download_key=NaN
 
 target/lib/dsdgen.jar: target/tools/dsdgen
 	cd target/; mkdir -p lib/; jar cvf lib/dsdgen.jar tools/
--- src/main/java/org/notmysock/tpcds/GenTable.java
+++ src/main/java/org/notmysock/tpcds/GenTable.java
@@ -31,10 +31,14 @@ public class GenTable extends Configured implements Tool {
 
     @Override
     public int run(String[] args) throws Exception {
-        String[] remainingArgs = new GenericOptionsParser(getConf(), args).getRemainingArgs();
+	Configuration conf = new Configuration();
+        String[] remainingArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+
+	String[] tables = "date_dim time_dim item customer customer_demographics household_demographics customer_address store promotion warehouse ship_mode reason income_band call_center web_page catalog_page inventory store_sales store_returns web_sales web_returns web_site catalog_sales catalog_returns".split(" ");
+	
 
         CommandLineParser parser = new BasicParser();
-        getConf().setInt("io.sort.mb", 4);
+        conf.setInt("io.sort.mb", 4);
         org.apache.commons.cli.Options options = new org.apache.commons.cli.Options();
         options.addOption("s","scale", true, "scale");
         options.addOption("t","table", true, "table");
@@ -66,44 +70,47 @@ public class GenTable extends Configured implements Tool {
           return 1;
         }
 
-        Path in = genInput(table, scale, parallel);
+	for (String t : tables)
+	    {
+		Path in = genInput(t, scale, parallel, conf);
 
-        Path dsdgen = copyJar(new File("target/lib/dsdgen.jar"));
-        URI dsuri = dsdgen.toUri();
-        URI link = new URI(dsuri.getScheme(),
+		Path dsdgen = copyJar(new File("target/lib/dsdgen.jar"));
+		URI dsuri = dsdgen.toUri();
+		URI link = new URI(dsuri.getScheme(),
                     dsuri.getUserInfo(), dsuri.getHost(), 
                     dsuri.getPort(),dsuri.getPath(), 
                     dsuri.getQuery(),"dsdgen");
-        Configuration conf = getConf();
-        conf.setInt("mapred.task.timeout",0);
-        conf.setInt("mapreduce.task.timeout",0);
-        DistributedCache.addCacheArchive(link, conf);
-        Job job = new Job(conf, "GenTable+"+table+"_"+scale);
-        job.setJarByClass(getClass());
-        job.setNumReduceTasks(0);
-        job.setMapperClass(DSDGen.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
 
-        job.setInputFormatClass(NLineInputFormat.class);
-        NLineInputFormat.setNumLinesPerSplit(job, 1);
+		conf.setInt("mapred.task.timeout",0);
+		conf.setInt("mapreduce.task.timeout",0);
+		DistributedCache.addCacheArchive(link, conf);
+		Job job = new Job(conf, "GenTable+"+t+"_"+scale);
+		job.setJarByClass(getClass());
+		job.setNumReduceTasks(0);
+		job.setMapperClass(DSDGen.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
 
-        FileInputFormat.addInputPath(job, in);
-        FileOutputFormat.setOutputPath(job, out);
+		job.setInputFormatClass(NLineInputFormat.class);
+		NLineInputFormat.setNumLinesPerSplit(job, 1);
+
+		FileInputFormat.addInputPath(job, in);
+		FileOutputFormat.setOutputPath(job, new Path(out.toString()+"/"+t));
 
         // use multiple output to only write the named files
-        LazyOutputFormat.setOutputFormatClass(job, TextOutputFormat.class);
-        MultipleOutputs.addNamedOutput(job, "text", 
-          TextOutputFormat.class, LongWritable.class, Text.class);
+        //LazyOutputFormat.setOutputFormatClass(job, TextOutputFormat.class);
+        //MultipleOutputs.addNamedOutput(job, "text", 
+	//TextOutputFormat.class, LongWritable.class, Text.class);*//
 
         boolean success = job.waitForCompletion(true);
 
         // cleanup
-        FileSystem fs = FileSystem.get(getConf());
+        FileSystem fs = FileSystem.get(conf);
         
         fs.delete(in, false);
+	    
         fs.delete(dsdgen, false);
-
+	    }
         return 0;
     }
 
@@ -121,22 +128,23 @@ public class GenTable extends Configured implements Tool {
       String md5hex = md5.toString(16);
       Path dst = new Path(String.format("/tmp/%s.jar",md5hex));
       Path src = new Path(jar.toURI());
-      FileSystem fs = FileSystem.get(getConf());
+      Configuration conf = new Configuration();
+      FileSystem fs = FileSystem.get(conf);
       fs.copyFromLocalFile(false, /*overwrite*/true, src, dst);
       return dst; 
     }
 
-    public Path genInput(String table, int scale, int parallel) throws Exception {
+    public Path genInput(String table, int scale, int parallel, Configuration conf) throws Exception {
         long epoch = System.currentTimeMillis()/1000;
-
+	
         Path in = new Path("/tmp/"+table+"_"+scale+"-"+epoch);
-        FileSystem fs = FileSystem.get(getConf());
+        FileSystem fs = FileSystem.get(new Configuration());
         FSDataOutputStream out = fs.create(in);
         for(int i = 1; i <= parallel; i++) {
           if(table.equals("all")) {
-            out.writeBytes(String.format("./dsdgen -dir $DIR -force Y -scale %d -parallel %d -child %d\n", scale, parallel, i));
+            out.writeBytes(String.format("./dsdgen -dir ./ -force Y -scale %d -parallel %d -child %d\n", scale, parallel, i));
           } else {
-            out.writeBytes(String.format("./dsdgen -dir $DIR -table %s -force Y -scale %d -parallel %d -child %d\n", table, scale, parallel, i));
+            out.writeBytes(String.format("./dsdgen -dir ./ -table %s -force Y -scale %d -parallel %d -child %d\n", table, scale, parallel, i));
           }
         }
         out.close();
@@ -150,31 +158,33 @@ public class GenTable extends Configured implements Tool {
       String read = br.readLine();
 
       while(read != null) {
-        //System.out.println(read);
         sb.append(read);
         read =br.readLine();
       }
       return sb.toString();
     }
 
-    static final class DSDGen extends Mapper<LongWritable,Text, Text, Text> {
+    static final class DSDGen extends Mapper<LongWritable,Text, NullWritable, Text> {
       private MultipleOutputs mos;
+	  private Text outputText = new Text();
       protected void setup(Context context) throws IOException {
-        mos = new MultipleOutputs(context);
-      }
-      protected void cleanup(Context context) throws IOException, InterruptedException {
-        mos.close();
+	  // mos = new MultipleOutputs(context);
+	  //}      protected void cleanup(Context context) throws IOException, InterruptedException {
+	  //mos.close();
+	  
       }
       protected void map(LongWritable offset, Text command, Mapper.Context context) 
         throws IOException, InterruptedException {
         String parallel="1";
         String child="1";
-
+       
         String[] cmd = command.toString().split(" ");
 
         for(int i=0; i<cmd.length; i++) {
           if(cmd[i].equals("$DIR")) {
-            cmd[i] = (new File(".")).getAbsolutePath();
+	      File cmd_f = new File("tpc_tmp_data");
+	      cmd_f.mkdirs();
+            cmd[i] = cmd_f.getAbsolutePath();
           }
           if(cmd[i].equals("-parallel")) {
             parallel = cmd[i+1];
@@ -183,33 +193,37 @@ public class GenTable extends Configured implements Tool {
             child = cmd[i+1];
           }
         }
-
+	System.out.println(command.toString());
         Process p = Runtime.getRuntime().exec(cmd, null, new File("dsdgen/tools/"));
-        int status = p.waitFor();
-        if(status != 0) {
-          String err = readToString(p.getErrorStream());
-          throw new InterruptedException("Process failed with status code " + status + "\n" + err);
-        }
-
-        File cwd = new File(".");
-        final String suffix = String.format("_%s_%s.dat", child, parallel);
-
-        FilenameFilter tables = new FilenameFilter() {
-          public boolean accept(File dir, String name) {
-            return name.endsWith(suffix);
-          }
-        };
-
-        for(File f: cwd.listFiles(tables)) {
-          BufferedReader br = new BufferedReader(new FileReader(f));          
-          String line;
-          while ((line = br.readLine()) != null) {
+	BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));          
+	String line;
+	while ((line = br.readLine()) != null) {
             // process the line.
-            mos.write("text", line, null, f.getName().replace(suffix,"/data"));
-          }
-          br.close();
-          f.deleteOnExit();
-        }
+	    outputText.set(line);
+	    context.write(NullWritable.get(), outputText);
+	}
+        int status = p.waitFor();	
+	br.close();
+
+
+        //if(status != 0) {
+        //  String err = readToString(p.getErrorStream());
+        //  throw new InterruptedException("Process failed with status code " + status + "\n" + err);
+        //}
+
+        //File cwd = new File("tpc_tmp_data");
+        //final String suffix = String.format("_%s_%s.dat", child, parallel);
+
+        //FilenameFilter tables = new FilenameFilter() {
+        //  public boolean accept(File dir, String name) {
+        //    return name.endsWith(suffix);
+        //  }
+        //};
+
+        //for(File f: cwd.listFiles(tables)) {
+        
+        //  f.deleteOnExit();
+      
       }
     }
 }
